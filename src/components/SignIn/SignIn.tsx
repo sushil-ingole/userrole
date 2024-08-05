991094d3eb51aa726e359fc33e4a1f9b66ba3a5d
Sushil Ingole <sushil.ingole@nihilent.com>
2024-08-05 19:11:07 +0530

    Added validation for signin

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -2,8 +2,11 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 import { IUserRole, role } from "../store/rolereducer";
 import "./SignIn.scss";
+import { useState } from "react";
 
 const SignIn = () => {
+    const [name, setName] = useState<string>("");
+    const [pass, setPass] = useState<string>("");
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const handleSignIn = async () => {
@@ -28,13 +31,13 @@ const SignIn = () => {
             <h4>Welcome Again!</h4>
             <div className="mb-3">
                 <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <input type="email" className="form-control" value={name} onChange={(e) => setName(e.target.value)} id="exampleInputEmail1" aria-describedby="emailHelp" />
             </div>
             <div className="mb-3">
                 <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" />
+                <input type="password" className="form-control" value={pass} onChange={(e) => setPass(e.target.value)} id="exampleInputPassword1" />
             </div>
-            <button type="button" className="btn btn-primary" onClick={handleSignIn}>Submit</button>
+            <button type="button" className="btn btn-primary" onClick={handleSignIn} disabled={name?.length <= 0 && pass?.length <= 0}>Submit</button>
         </form>
     )
 }
